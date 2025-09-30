a3cec7d15b92dabd264725ac61032435cacd0b0c
hwanyseo <hwanyseo@cubrid.com>
2025-10-01 07:29:07 +0900

    update python batch file

--- python-odbc/run_test.bat
+++ python-odbc/run_test.bat
@@ -1,12 +1,20 @@
 @echo off
 
-for /f "tokens=*" %%i in ('pip list ^| findstr pyodbc') do set PYODBC_VERSION=%%i
+set PYTHON36_PATH=C:\Python\Python36
+set PYTHON310_PATH=C:\Python\Python310
+set PYTHON311_PATH=C:\Python\Python311
+set PYTHON312_PATH=C:\Python\Python312
+set USE_PYTHON_PATH=%PYTHON312_PATH%
+set PYTHON_PATH=%USE_PYTHON_PATH%\python.exe
+set PIP_PATH=%USE_PYTHON_PATH%\Scripts\pip.exe
+
+for /f "tokens=*" %%i in ('%PIP_PATH% list ^| findstr pyodbc') do set PYODBC_VERSION=%%i
 
 if "%PYODBC_VERSION%" neq "pyodbc  5.2.0" (
     echo "pyodbc 5.2.0 is not installed"
-    pip install pyodbc==5.2.0
+    %PIP_PATH% install pyodbc==5.2.0
 ) else (
     echo "pyodbc 5.2.0 is installed"
 )
 
-python odbc.py
+%PYTHON_PATH% odbc.py
