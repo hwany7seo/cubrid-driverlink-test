1118d659676ecd700c7e3c9c5d735a3e43a5241a
hwanyseo <hwanyseo@cubrid.com>
2026-03-21 11:51:06 +0900

    complete nodejs

---
+++ node-jdbc/to-nodejs-jdbc/cubrid-test/ErrorMessages.cjs
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = {
+  ERROR_CLOSED_CONNECTION_COMMIT: 'Cannot commit: connection is closed.',
+
+  ERROR_AUTO_COMMIT_ENABLED_COMMIT:
+    'Cannot commit while auto-commit is enabled.',
+
+  ERROR_NO_ROLLBACK:
+    'Cannot rollback: connection is not available or auto-commit is enabled.',
+};
