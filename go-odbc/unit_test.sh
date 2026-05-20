3869471e64d296635e63b5801319bc168009ae8e
hwanyseo <hwanyseo@cubrid.com>
2026-05-20 15:02:24 +0900

    go odbc test

--- go-odbc/unit_test.sh
+++ go-odbc/unit_test.sh
@@ -2,6 +2,6 @@
 SHELL_PATH=$(pwd)
 GO_GET_PATH=$(go env GOPATH)
 echo "GO_GET_PATH: $GO_GET_PATH"
-go run to_odbc/bind_test.go
+go run to_odbc/bind.go
 go run to_odbc/example.go
 go run to_odbc/olympic.go
